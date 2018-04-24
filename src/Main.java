b59e4b7af9ee7689545c505f543c52813bcabed9
BenGround <benjaminhartmann.contact@gmail.com>
2018-04-24 09:52:40 +0200

    main

--- src/Main.java
+++ src/Main.java
@@ -6,12 +6,7 @@ import jpu2016.dogfight.view.DogfightView;
 public class Main {
 
     public static void main(String args[]) {
-    	final DogfightModel dogfightModel = new DogfightModel();
-    	final DogfightController dogfightController = new DogfightController(dogfightModel);
-    	final DogfightView dogfightView = new DogfightView(dogfightController, dogfightModel, dogfightModel);
-    	dogfightController.setViewSystem(dogfightView);
-    	dogfightController.play();
+
     }
-}
 
 }
